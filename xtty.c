c156d1a1b4f2f35a228d7e62e7ffd4b981b2dee7
Serhii Kuzmychov <ku3cmich@gmail.com>
2024-01-18 01:36:49 +0200

    sync

--- xtty.c
+++ xtty.c
@@ -1,21 +1,63 @@
 #include "xtty.h"
-#include <zephyr/drivers/uart.h>
+#include <xlog.h>
 
-void xtty_irq(const struct device *dev, void *user_data)
+#include <zephyr/logging/log.h>
+#include <string.h>
+
+LOG_MODULE_REGISTER(xtty, LOG_LEVEL_DBG);
+
+void xtty_irq(const struct device *dev, struct uart_event *evt, void *data)
 {
-    uart_irq_update(dev);
-
-    tty_spec_t *spec = user_data;
+    tty_spec_t *spec = data;
     if (dev != spec->uart)
         return;
 
-    while (uart_irq_rx_ready(dev))
+    LOG_DBG("xtty_irq: evt: %u", evt->type);
+    queue_msg_t *msg;
+
+    switch (evt->type)
     {
-        queue_msg_t *msg = alloc_queue_msg(spec->recv);
-        msg->len = uart_fifo_read(dev, msg->data, spec->recv->msg_max_length);
-        if (!msg->len)
-            free_queue_msg(spec->recv, msg);
-        else
-            k_queue_append(spec->recv->queue, msg);
+    case UART_RX_RDY:
+        struct uart_event_rx *rx = &evt->data.rx;
+        if (rx->len)
+        {
+            msg = alloc_queue_msg(spec->recv);
+            msg->len = rx->len;
+            memcpy(msg->data, rx->buf + rx->offset, rx->len);
+
+            k_queue_alloc_append(spec->recv->queue, msg);
+        }
+        break;
+
+    case UART_RX_BUF_REQUEST:
+        __ASSERT(spec->ovfw_assigned == 0, "UART_RX_BUF_REQUEST, ovfw_buf already assigned");
+        LOG_DBG("UART_RX_BUF_REQUEST");
+
+        struct uart_event_rx_buf *rx_buf = &evt->data.rx_buf;
+        rx_buf->buf = spec->ovfw_buf;
+        spec->ovfw_assigned = 1;
+        break;
+
+    case UART_RX_BUF_RELEASED:
+        LOG_DBG("UART_RX_BUF_RELEASED");
+        spec->ovfw_assigned = 0;
+        break;
+
+    case UART_RX_DISABLED:
+        // todo: remove dup from xtty_init
+        uart_rx_enable(dev, spec->recv_buf, spec->recv->msg_max_length, 10);
+        break;
     }
+}
+
+int xtty_init(tty_spec_t *xtty, const struct uart_config *spec)
+{
+    ASSERT_RESULT(uart_configure, xtty->uart, spec);
+    ASSERT_RESULT(uart_callback_set, xtty->uart, xtty_irq, xtty);
+    xtty->recv_buf = k_malloc(xtty->recv->msg_max_length);
+    xtty->ovfw_buf = k_malloc(xtty->recv->msg_max_length);
+    xtty->ovfw_assigned = 0;
+
+    // todo: timeout to spec
+    return uart_rx_enable(xtty->uart, xtty->recv_buf, xtty->recv->msg_max_length, 10);
 }
--- xtty.h
+++ xtty.h
@@ -2,15 +2,19 @@
 #define __xtty_h__
 
 #include <xqueue.h>
+#include <zephyr/drivers/uart.h>
 
 typedef struct
 {
     const struct device *uart;
     queue_data_spec_t *recv;
     queue_data_spec_t *send;
+    uint8_t *recv_buf;
+    uint8_t *ovfw_buf;
+    uint8_t ovfw_assigned;
 } tty_spec_t;
 
-void xtty_irq(const struct device *dev, void *user_data);
+int xtty_init(tty_spec_t *xtty, const struct uart_config *spec);
 
 #define DEFINE_XTTY_DATA(_name, _msg_size, _msgs_limit)        \
     DEFINE_QUEUE_DATA(_name##_recv_q, _msg_size, _msgs_limit); \
@@ -19,12 +23,9 @@ void xtty_irq(const struct device *dev, void *user_data);
 #define DEFINE_XTTY(_queue, _tty_dev)                                              \
     const queue_data_spec_t _queue##_recv_spec = INIT_QUEUE_SPEC(_queue##_recv_q); \
     const queue_data_spec_t _queue##_send_spec = INIT_QUEUE_SPEC(_queue##_send_q); \
-    tty_spec_t _queue = {                                                    \
+    tty_spec_t _queue = {                                                          \
         .uart = _tty_dev,                                                          \
         .recv = &_queue##_recv_spec,                                               \
-        .send = &_queue##_send_spec};                                              \
-    uart_irq_callback_user_data_set(_tty_dev, xtty_irq, &_queue);                  \
-    uart_irq_rx_enable(_tty_dev);                                                  \
-    uart_irq_tx_enable(_tty_dev)
+        .send = &_queue##_send_spec}
 
 #endif
